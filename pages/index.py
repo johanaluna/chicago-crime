b75848366e7867e8f834523b90ab20fee9dd0ee5
Johana Luna <johana.luna87@gmail.com>
2019-09-27 12:00:17 -0400

    final notebook_link

--- pages/index.py
+++ pages/index.py
@@ -60,9 +60,6 @@ problem = dbc.Row(
              and an interesting case to analyze. The vast majority of these killings happened in five mostly black and 
              Latino neighborhoods on the south and west side where only 9% of the 2.7m city lives[2](https://www.economist.com/democracy-in-america/2017/09/26/chicago-accounted-for-22-of-a-nationwide-increase-in-murders-last-year)
             
-
-            
-            [Documentation and NoteBook here](https://github.com/johanaluna/Chicago)
             """
         ),
 
@@ -78,11 +75,21 @@ graphs = dbc.Row([
     ],
 )
 
+link_github = dbc.Row(
+    [
+        dcc.Markdown(
+            """
+            [Documentation and NoteBook here](https://github.com/johanaluna/Chicago)
+            """
+        ),
+
+    ],
+)
 empty_rows= dbc.Row([
     dbc.Col(
         html.Div(' ',style={'text-align': 'center','font-size':16,'font-weight':'bold','height':'36px'})
     )
 ])
-column_list=[introduction, empty_rows,problem,graphs]
+column_list=[introduction, empty_rows,problem,graphs, link_github]
 
 layout = dbc.Col(column_list)
